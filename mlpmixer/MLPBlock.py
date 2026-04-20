2a4158881162c84c8b9e7506b66573da3988548c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-20 14:16:45 +0200

    adds mlp block

---
+++ mlpmixer/MLPBlock.py
@@ -0,0 +1,16 @@
+import torch
+from typing import Type
+
+
+class MLPBlock(torch.nn.Module):
+    def __init__(self, *, input_dimension: int, hidden_dimension: int, activation: Type[torch.nn.Module] = torch.nn.GELU):
+        super().__init__()
+        self.mu = torch.nn.Linear(input_dimension, hidden_dimension)
+        self.nu = torch.nn.Linear(hidden_dimension, input_dimension)
+        self.xi = activation()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.mu(x)
+        x = self.xi(x)
+        x = self.nu(x)
+        return x
