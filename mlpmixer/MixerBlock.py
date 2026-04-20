ebfc62b4e401d65901a1daa1eaa670057afee1e0
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-20 14:22:30 +0200

    adds mixer block

---
+++ mlpmixer/MixerBlock.py
@@ -0,0 +1,23 @@
+import torch
+from .MLPBlock import MLPBlock
+
+class MixerBlock(torch.nn.Module):
+    def __init__(self, *, token_dimension: int, hidden_dimension: int, token_mlp_dimension: int, channels_mlp_dimension: int):
+        super().__init__()
+        self.token_mlp = MLPBlock(token_dimension, token_mlp_dimension)
+        self.channel_mlp = MLPBlock(hidden_dimension, channels_mlp_dimension)
+        self.alpha = torch.nn.LayerNorm(hidden_dimension)
+        self.beta = torch.nn.LayerNorm(hidden_dimension)
+
+    def forward(self, x):
+        residual = x
+        x = self.alpha(x).transpose(1, 2)
+        x = self.token_mlp(x).transpose(1, 2)
+
+        x = residual + x
+
+        residual = x
+        x = self.beta(x)
+        x = self.channel_mlp(x)
+
+        return residual + x
