af0ef42a4cff2bcb6f0d0d3e3ea2aad7534b112a
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 00:01:51 +0200

    adds mlp jax block

---
+++ mixers/jax/modules/MLPBlock.py
@@ -0,0 +1,16 @@
+import jax
+from flax import linen as nn
+from typing import Type
+
+
+class MLPBlock(nn.Module):
+    input_dimension: int
+    hidden_dimension: int
+    activation: Type[nn.Module]
+
+    @nn.compact
+    def __call__(self, x: jax.numpy.ndarray) -> jax.numpy.ndarray:
+        x = nn.Dense(self.hidden_dimension)(x)
+        x = self.activation(x)
+        x = nn.Dense(self.input_dimension)(x)
+        return x
