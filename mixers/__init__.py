054babbb8a2fb1bd35b7bf04c1f7da98fb70160c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-19 11:55:47 +0200

    version

--- mixers/__init__.py
+++ mixers/__init__.py
@@ -1,10 +1,9 @@
 from .BaseMixer16 import BaseMixer16
 from .BaseMixer32 import BaseMixer32
-
 from .SmallMixer16 import SmallMixer16
 from .SmallMixer32 import SmallMixer32
-
 from .LargeMixer16 import LargeMixer16
 from .LargeMixer32 import LargeMixer32
-
 from .HugeMixer14 import HugeMixer14
+
+__version__ = "0.0.12"
