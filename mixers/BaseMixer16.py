09dcb91baa9ad2740695d6d9a0905a26078af702
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-19 13:04:52 +0200

    adds smoke tests to mixers

--- mixers/BaseMixer16.py
+++ mixers/BaseMixer16.py
@@ -24,3 +24,15 @@ class BaseMixer16(MLPMixer):
             channel_mixing_activation=channel_mixing_activation,
             token_mixing_activation=token_mixing_activation,
         )
+
+
+if __name__ == "__main__":
+    model = BaseMixer16(num_classes=10, image_size=224)
+    images = torch.randn(1, 3, 224, 224)
+    predictions = model(images)
+
+    assert predictions.shape[-1] == 10
+
+    parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Number of parameters (in millions): {parameters / pow(10, 6)}")
+    print(f"Prediction shape: {predictions.shape}")
