c159ef61a07894612f85b4c7508be93081971b51
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-19 12:19:12 +0200

    fixes mlp mixer

--- mixers/modules/MLPMixer.py
+++ mixers/modules/MLPMixer.py
@@ -1,5 +1,6 @@
 import torch
 import einops
+from typing import Tuple, Union
 from .MixerBlock import MixerBlock
 from .HeadModule import HeadModule
 
@@ -8,53 +9,63 @@ class MLPMixer(torch.nn.Module):
     def __init__(
         self,
         *,
+        in_channels: int,
+        image_size: Union[int, Tuple[int, int]],
+        patch_size: Union[int, Tuple[int, int]],
         num_classes: int,
-        image_size: int,
         num_blocks: int,
         hidden_dimension: int,
-        tokens_mlp_dimension: int,
-        channels_mlp_dimension: int,
-        patch_size: int,
-        image_channels: int,
+        tokens_mlp_dim: int,
+        channels_mlp_dim: int,
     ):
         super().__init__()
+        self.num_classes = num_classes
 
-        self.patch_size = patch_size
-        self.num_blocks = num_blocks
+        if isinstance(image_size, int):
+            image_size = (image_size, image_size)
 
-        self.stem = torch.nn.Conv2d(
-            in_channels=image_channels,
-            out_channels=hidden_dimension,
-            kernel_size=patch_size,
-            stride=patch_size,
+        if isinstance(patch_size, int):
+            patch_size = (patch_size, patch_size)
+
+        self.sequence_length = (image_size[0] // patch_size[0]) * (
+            image_size[1] // patch_size[1]
         )
 
-        num_patches = (image_size // patch_size) ** 2
+        self.patch_embedding = torch.nn.Conv2d(
+            in_channels, hidden_dimension, kernel_size=patch_size, stride=patch_size
+        )
 
         self.blocks = torch.nn.ModuleList(
             [
                 MixerBlock(
-                    num_tokens=num_patches,
-                    hidden_dimension=hidden_dimension,
-                    tokens_mlp_dimension=tokens_mlp_dimension,
-                    channels_mlp_dimension=channels_mlp_dimension,
+                    sequence_length=self.sequence_length,
+                    channels=hidden_dimension,
+                    tokens_mlp_dim=tokens_mlp_dim,
+                    channels_mlp_dim=channels_mlp_dim,
                 )
                 for _ in range(num_blocks)
             ]
         )
 
-        self.normalization = torch.nn.LayerNorm(hidden_dimension)
-        self.head = HeadModule(
-            hidden_dimension=hidden_dimension, num_classes=num_classes
-        )
+        self.final_normalization = torch.nn.LayerNorm(hidden_dimension)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.stem(x)
+        if self.num_classes > 0:
+            self.head = HeadModule(hidden_dimension=hidden_dimension)
+
+    def aggregate(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.mean(x, dim=1)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = self.patch_embedding(inputs)
         x = einops.rearrange(x, "n c h w -> n (h w) c")
 
         for block in self.blocks:
             x = block(x)
 
-        x = self.normalization(x)
-        x = x.mean(dim=1)
-        return self.head(x)
+        x = self.final_normalization(x)
+        x = self.aggregate(x)
+
+        if self.num_classes > 0:
+            x = self.head(x)
+
+        return x
